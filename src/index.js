6d5408d15c5201f8fd8403abc5660f7427eac17a
Jared Huffstutler <jaredhuff85@gmail.com>
2022-01-13 23:04:24 -0600

    Routing
    * Add test routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+
+// Components.
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App';
+import { Route1, Route2 } from './routes'
+
+// Utils, styles, and other helpers.
+import { render } from 'react-dom';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/route1" element={<Route1 />} />
+        <Route path="/route2" element={<Route2 />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/routes/route1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Route1() {
+  return (
+    <main>
+      <h2>Route 1</h2>
+    </main>
+  )
+}
